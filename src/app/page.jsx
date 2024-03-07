d38f67d4bce1b794cac41c0c8735f0acc0fc7bed
jrmart12 <rodolfojose@live.com>
2024-03-07 12:12:40 -0600

    page transition with framer motion

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,48 @@
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <motion.div
+      className="h-full "
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-30">
+        {/* Image container */}
+        <div className="h-1/2 relative lg:h-full lg:w-1/2">
+          <Image
+            src="/cartoon-rounded.png"
+            alt=""
+            fill
+            className="object-contain p-8"
+          />
+        </div>
+        {/* Text container */}
+        <div className="h-1/2 flex flex-col gap-8 items-start justify-center lg:h-full lg:w-1/2">
+          {/* Title */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Front End Developer
+          </h1>
+          {/* Description */}
+          <p className="md:text-xl">
+            I'm Jose Rodolfo Martinez a Computer Systems Engineer based in
+            Honduras
+          </p>
+          {/* Buttons */}
+          <div className="w-full flex gap-4">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </button>
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default Homepage;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,143 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import NavLink from "./navLink";
+import { motion } from "framer-motion";
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const topVariant = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+  const centerVariant = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const bottomVariant = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  const listVariant = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listItemVariant = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
+    },
+  };
+
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-30">
+      {/*Links on md */}
+      <div className="hidden md:flex gap-4 w-1/3 text-xl">
+        {links.map((link) => (
+          <NavLink link={link} key={link.title} />
+        ))}
+      </div>
+      {/*Logo */}
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mr-1">Jose </span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
+        </Link>
+      </div>
+      {/*Links social */}
+      <div className="hidden md:flex flex-row gap-4 w-1/3 justify-end">
+        <Link href="https://github.com/jrmart12">
+          <Image src="/github.png" width={24} height={24} alt="" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/jose-martinez-706253100/">
+          <Image src="/linkedin.png" width={24} height={24} alt="" />
+        </Link>
+      </div>
+      {/*Hamburger menu on sm */}
+      <div className="md:hidden">
+        <button
+          className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setOpen(!open)}
+        >
+          <motion.div
+            variants={topVariant}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
+          <motion.div
+            variants={centerVariant}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={bottomVariant}
+            animate={open ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
+        </button>
+        {open && (
+          <motion.div
+            variants={listVariant}
+            animate={"opened"}
+            initial={"closed"}
+            className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-10"
+          >
+            {links.map((link) => (
+              <motion.div
+                variants={listItemVariant}
+                className=""
+                key={link.title}
+              >
+                <Link href={link.url}>{link.title}</Link>
+              </motion.div>
+            ))}
+          </motion.div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
